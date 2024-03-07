57febdeb3a541df4a09c73cc0f685828b5c0cfe7
utkarshInnoraft73 <utkarsh.singh@innoraft.com>
2024-03-07 18:15:37 +0530

    FT2-37-3: Handle Errors for frontend validation. Description: - Add frontend validation. - Add OOPs concept.

--- index.php
+++ index.php
@@ -1,188 +1,133 @@
+<?php
+require("user.php");
+/*
+    Variable for error message.
+    fnameErr = for first name error.Invalid Input.
+    lnameErr = for last name error.
+    txteraErr = for textarea error.
+    */
+$fnameErr = $lnameErr = $txteraErr = "";
+
+/*
+    Function for Validation.
+    nameValidation for validating name.
+    */
+function nameValidation($data, &$errorMsg)
+{
+    if (empty($data)) {
+        $errorMsg = "This field is required";
+        return "";
+    } 
+    else {
+        $name = test_input($data);
+        if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
+            $errorMsg = "Only letters are allowed.";
+            return "";
+        } 
+        else if (empty($name)) {
+            $errorMsg = "Invalid Input.";
+            return "";
+        } 
+        else {
+            return $name;
+        }
+    }
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $fname = nameValidation($_POST['fname'], $fnameErr);
+    $lname = nameValidation($_POST['lname'], $lnameErr);
+
+    //   Accessing the Images.
+    $imgName = $_FILES['image']['name'];
+    $img_temp_name = $_FILES['image']['tmp_name'];
+    move_uploaded_file($img_temp_name, "Uploads/$imgName");
+
+    // Accessing Subject and marks.
+    if (!empty($_POST['marks'])) {
+        // Converting the string into array by new line.
+        $marks_lines = explode("\n", $_POST['marks']);
+    }
+}
+
+/**Testing input data. */
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+// Message to show after successfull submit the form 
+if (!empty($fname) && !empty($lname)) {
+    $message = "Hello, {$fname} {$lname}.";
+}
+
+
+$user = new User($fname, $lname);
+?>
+
 <!doctype html>
 <html lang="en">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Assignment</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <script src="index.js"></script>
 </head>
-<style>
-    .error {
-        color: red;
-    }
-</style>
 
 <body>
-
-    <?php
-
-    $fname = $lname = "";
-    $fnameErr = $lnameErr = "";
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Checking if the First name field only contains letters, dashes, apostrophes and whitespaces.
-        if (empty($_POST["fname"])) {
-            $fnameErr = "This field is required";
-        } else if (!preg_match("/^[a-zA-Z-' ]*$/", $_POST["fname"])) {
-            $fnameErr = "Only letters and white space allowed";
-        } else {
-            $fname = test_input($_POST["fname"]);
-        }
-
-        // Checking if the Last name field only contains letters, dashes, apostrophes and whitespaces.
-
-        if (empty($_POST["lname"])) {
-            $lnameErr = "This field is required";
-        } else if (!preg_match("/^[a-zA-Z-' ]*$/", $_POST["lname"])) {
-            $lnameErr = "Only letters and white space allowed";
-        } else {
-            $lname = test_input($_POST["lname"]);
-        }
-
-
-
-        //   Accessing the Images
-        $imgName = $_FILES['image']['name'];
-        $img_temp_name = $_FILES['image']['tmp_name'];
-        move_uploaded_file($img_temp_name, "Uploads/$imgName");
-
-
-        // Accessing Subject and marks
-
-        if (!empty($_POST['marks'])) {
-
-            // Converting the string into array by new line.
-            $marks_lines = explode("\n", $_POST['marks']);
-           
-        }
-        $errMsg = "Marks is not entered.";
-    }
-
-    function test_input($data)
-    {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
-
-    // Making the person class
-
-    class Person
-    {
-        public $fname;
-        public $lname;
-
-        // Creating the cunstructor
-        function __construct($fname, $lname)
-        {
-            $this->fname = $fname;
-            $this->lname = $lname;
-        }
-
-        // Getting First name
-        function get_firstName()
-        {
-            return $this->fname;
-        }
-
-        // Getting Last name
-        function get_lastName()
-        {
-            return $this->lname;
-        }
-    }
-
-    $person = new Person($fname, $lname);
-    ?>
-
-    <div class="container py-5">
-
-        <div class="card mt-5">
-            <div class="card-header">
-                Personal Details.
-            </div>
-            <!-- After given input, The message will be show -->
-            <?php
-            if (!empty($person->get_firstName()) || !empty($person->get_lastName())) {
-                echo "<h3 class='mt-3 ms-3'>Hello, {$person->get_firstName()} {$person->get_lastName()}</h3>";
-            }
-            ?>
-            <div class="card-body">
-                <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" enctype="multipart/form-data">
-                    <div class="mb-3">
-                        <label for="" class="form-label">First name
-                            <span class="error">*<?php echo "{$fnameErr}" ?><span>
+    <div class="container">
+        <div class="row d-flex justify-content-center my-5">
+            <h1 class="text-center my-2"><?php echo $message; ?></h1>
+            <div class="col-6">
+                <form class="row g-3 needs-validation" action="<?php echo $_SERVER['PHP_SELF']; ?>" m ethod="post" enctype="multipart/form-data" onsubmit="return checkInputs()">
+                    <div class="col-md-8">
+                        <label for="validationCustom01" class="form-label">First name
+                            <span class="require" id="fnameErr">*</span>
                         </label>
-                        <input type="text" class="form-control" id="" aria-describedby="" name="fname">
+                        <input type="text" class="form-control item" id="fname" name="fname" value="" minlength="3" maxlength="20" required>
                     </div>
-                    <div class="mb-3">
-                        <label for="" class="form-label">Last name
-                            <span class="error">*<?php echo "{$lnameErr}" ?><span>
-
+                    <div class="col-md-8">
+                        <label for="validationCustom02" class="form-label">Last name
+                            <span class="require" id="lnameErr">*</span>
                         </label>
-                        <input type="text" class="form-control" id="" aria-describedby="" name="lname">
+                        <input type="text" class="form-control item" id="lname" name="lname" value="" minlength="3" maxlength="20" required>
                     </div>
-                    <div class="mb-3">
-                        <label for="" class="form-label">Upload image</label>
-                        <input type="file" class="form-control" id="" aria-describedby="" name="image">
+
+                    <div class="col-md-8">
+                        <label for="validationCustom02" class="form-label">Upload image
+                        </label>
+                        <input type="file" class="form-control item" name="image" accept="image/*" required>
                     </div>
-                    <div class="mb-3">
-                        <label for="" class="form-label">Subject and marks (ex: English|80)</label>
-                        <textarea type="text" class="form-control" id="" aria-describedby="" name="marks" rows="4" cols=""></textarea>
+                    <div class="col-md-8">
+                        <label for="floatingTextarea2">Subject marks (Format: Subject|Marks)
+                            <span class="require" id="marksErr">*</span>
+                        </label>
+                        <div class="form-floating">
+                            <textarea class="form-control" id="marks" style="height: 100px" name="marks" required></textarea>
+                        </div>
                     </div>
-                    <input type="submit" class="btn btn-primary">
+                    <div class="col-12">
+                        <input type="submit" name="submit">
+                    </div>
                 </form>
             </div>
             <div style="width: 100% !important;">
                 <?php
-                if (!empty($imgName)) {
-                    echo "<img src='Uploads/{$imgName}' height='400' width='400' style='display: block; margin: auto;'>
-                <h4 style='margin: 10px 0; text-align:center;'><?php echo '{$person->get_firstName()} {$person->get_lastName()}'; ?></h4>";
+                if (!empty($imgName)) { ?>
+                    <img src="<?php echo 'Uploads/{$imgName}'; ?>" height='400' width='400' style='display: block; margin: auto;'>
+                    <h4 style='margin: 10px 0; text-align:center;'><?php echo "{$user->getFirstName()} {$user->getLastName()}"; ?></h4>
+                <?php
                 }
                 ?>
             </div>
-
-
-            <div>
-
-                <h4 style="margin: 10px 0; text-align:center;"> Entered Marks</h4>
-
-                <table class="table table-hover">
-                    <thead>
-                        <tr class="table-dark">
-                            <th scope="col">#</th>
-                            <th scope="col">Subject</th>
-                            <th scope="col">Marks</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        $i = 1;
-
-                        foreach ($marks_lines as $lines) {
-                            // exploding marks_line as subject and marks seperating by |
-                            $parts = explode("|", $lines);
-                            $subject = trim($parts[0]);
-                            $mark = trim($parts[1]);
-
-                            // printing subject and marks
-                            echo "<tr>
-                    <td>{$i}</td>
-                    <td>{$subject}</td>
-                    <td>{$mark}</td>
-                    
-                  </tr>";
-                            $i = $i + 1;
-                        }
-                        ?>
-
-                    </tbody>
-                </table>
-            </div>
         </div>
-    </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
---
+++ user.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Make a user class to perform all the functionality.
+ */
+
+class User {
+    public $fname; //First name of user.
+    public $lname; //Last name of user.
+
+    /**
+     * Create a custructor.
+     * Setting first name, last name.
+     */
+    function __construct($fname, $lname)
+    {
+        $this->fname = $fname;
+        $this->lname = $lname;
+       
+    }
+
+
+    
+    /**
+     * Create functions to return the first name, last name.
+     */
+    function getFirstName()
+    {
+        return $this->fname;
+    }
+
+    function getLastName()
+    {
+        return $this->lname;
+    }
+
+   
+}
+?>
